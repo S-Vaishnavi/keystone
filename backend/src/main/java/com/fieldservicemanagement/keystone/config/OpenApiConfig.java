5f915240ac21360544fa200069e5af541454d073
J-Prem <premjosephdgl@gmail.com>
2026-08-06 16:03:36 +0530

    feat(swagger): configure OpenAPI bearer authentication

--- backend/src/main/java/com/fieldservicemanagement/keystone/config/OpenApiConfig.java
+++ backend/src/main/java/com/fieldservicemanagement/keystone/config/OpenApiConfig.java
@@ -2,6 +2,9 @@ package com.fieldservicemanagement.keystone.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -13,12 +16,20 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("KEYSTONE Field Service Management API")
+                        .version("v1.0")
+                        .description("REST API documentation for the KEYSTONE Field Service Management Platform.")
+                        .contact(new Contact()
+                                .name("KEYSTONE Development Team")
+                                .email("support@keystone.com"))
+                        .license(new License().name("Apache 2.0")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
